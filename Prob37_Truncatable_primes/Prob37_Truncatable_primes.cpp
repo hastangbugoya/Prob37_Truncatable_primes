77d016c4821c7e0fb308637d65c9e78ed4b781f9
Jeffrey Joseph <jjrzacal@outlook.com>
2019-09-23 08:30:28 -0700

    with output

--- Prob37_Truncatable_primes/Prob37_Truncatable_primes.cpp
+++ Prob37_Truncatable_primes/Prob37_Truncatable_primes.cpp
@@ -1,4 +1,4 @@
-// Prob37_Truncatable_primes.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// =>748317 Prob37_Truncatable_primes.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include "pch.h"
@@ -39,14 +39,18 @@ int main()
 	}
 	printf("\n\n=>%lld", bigsum);
 }
+/*
+23
+37
+53
+73
+313
+317
+373
+797
+3137
+3797
+739397
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+=>748317
+*/
